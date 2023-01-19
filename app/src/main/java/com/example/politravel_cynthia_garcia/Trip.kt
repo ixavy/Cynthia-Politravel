e5b30af491aa3238b1aa5a9bad00d5527d83905a
cyngargon <98019023+cyngargon@users.noreply.github.com>
2023-01-19 08:45:25 +0100

    Politravel anterior esborrat

--- app/src/main/java/com/example/politravel_cynthia_garcia/Trip.kt
+++
@@ -1,7 +0,0 @@
-package com.example.politravel_cynthia_garcia
-
-class Trip (val startingPointName: String,
-            val endingPointName: String,
-            val days: Int,
-            val startingPointCoordinates: Int,
-            val itineraryStopsName: MutableList<String>)
