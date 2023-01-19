56aeb82af777cd8f197d5d6db1bd0069574d4038
cyngargon <98019023+cyngargon@users.noreply.github.com>
2023-01-19 13:00:24 +0100

    ns

--- app/src/main/java/com/example/politravel/PaquetsAdapter.kt
+++ app/src/main/java/com/example/politravel/PaquetsAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
 class PaquetsAdapter(context: Context, val layout: Int, val paquets: MutableList<Paquet>):
     ArrayAdapter<Paquet>(context, layout, paquets) {
@@ -23,5 +25,16 @@ class PaquetsAdapter(context: Context, val layout: Int, val paquets: MutableList
 
     fun bindPaquet(view: View, paquet: Paquet){
         //TODO: 10 Llistes personalitzades 18:40
+        val imgPaquet = view.findViewById<ImageView>(R.id.item_img_paquet)
+        imgPaquet.setImageResource(paquet.imgLow)
+
+        val paquetName = view.findViewById<TextView>(R.id.item_name)
+        paquetName.text = paquet.name
+
+        val transport = view.findViewById<ImageView>(R.id.item_img_transport)
+        transport.setImageResource(paquet.transport)
+
+        val days = view.findViewById<TextView>(R.id.days_lst)
+        days.text = paquet.days.toString()
     }
 }
