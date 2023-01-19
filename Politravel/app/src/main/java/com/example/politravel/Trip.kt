d4d01dcb407b8b609dbdefee763e585f427854c4
cyngargon <98019023+cyngargon@users.noreply.github.com>
2023-01-19 08:58:05 +0100

    Carpeta Politravel eliminada

--- Politravel/app/src/main/java/com/example/politravel/Trip.kt
+++
@@ -1,7 +0,0 @@
-package com.example.politravel
-
-class Trip (val startingPointName: String,
-           val endingPointName: String,
-           val days: Int,
-           val startingPointCoordinates: Int,
-           val itineraryStopsName: MutableList<String>)
